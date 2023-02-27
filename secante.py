c1054766eac913703150bd641c07f301d682691c
Joaof14 <joaopedro140003@gmail.com>
2023-02-27 11:52:19 -0300

    'delete secante.py'

--- secante.py
+++
@@ -1,8 +0,0 @@
-if self.i >= 2:
-    fx.append(self.fx)
-    xk = x[self.i-1]-((self.fx* (x[self.i-1]-x[self.i-2]))/(fx[self.i-1]-fx[self.i - 2]))
-    print(xk)
-    x.append(xk)
-elif self.i == 0: 
-    x.append(self.a)
-    x.append(self.b)
