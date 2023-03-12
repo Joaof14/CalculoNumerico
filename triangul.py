35a7333f8ebc92d64cfcf539ef4520e2d874bab7
Joaof14 <joao-pedro032011@hotmail.com>
2023-03-12 16:18:52 -0300

    'adicionar comentarios'

--- triangul.py
+++ triangul.py
@@ -13,14 +13,18 @@ def outputTeste():
 def EliminGauss():
     global A
     for i in range(len(A)):
+        #define pivo e linha para utilizar com multiplicador operações
         pivo = A[i][i]
         linha = A[i]
         for j in range(i + 1,len(A)):
+            #define multiplicador da linha
             m = A[j][i] / pivo
             for k in range(i, len(A)):
+                #atualiza elementos da linha j com operações elementares entre 
+                #ela, multiplicador e elementos da linha anteriormente definidos
                 A[j][k] =  round(A[j][k] - m*linha[k], 3)
+            #atualiza vetor b para manter equivalência do sistema linear
             B[j] = round(B[j] - m*B[i], 3)
-        
-
     outputTeste()
+
 EliminGauss()
