9db3461d1f9d4ea7919eb9044008f693d1d5bfde
Joaof14 <joao-pedro032011@hotmail.com>
2023-03-13 12:29:07 -0300

    ''

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 a = [1,2,3]
 b = [4,5,6]
-print(a + b)
+c = [a,b]
+
+d = []
+for i in range(len(c)):
+    dlinha = []
+    for j in range(len(c[0])):
+        if i == j:
+            dlinha.append(1)
+        else:
+            dlinha.append(0)
+    d.append(dlinha)
+
+
+
+
+print(c)
+print(d)
