6f2d5bcb7067d7836f846d5c274dd073d555abad
Joaof14 <joao-pedro032011@hotmail.com>
2023-03-14 18:38:49 -0300

    ''

--- RS.py
+++ RS.py
@@ -1,27 +1,29 @@
-# Método da retrosubstituição:
-def retrosub(matriz_equacao):
+A = [[3,2,4],[0,2,2],[0,0,-2]]
+#A = [[3,5,9,4],[0,0,1,5],[0,3,2,3],[0,9,7,4]]
+B = [1,2,3]
+#B = [7,1,6,8]
 
-    n = len(matriz_equacao)
+
+# Método da retrosubstituição:
+def retrosub():
+    global A
+    global B
+    n = len(A)
     y = n*[0]
     
     for i in range(n-1, -1, -1):
         soma = 0
 
         for j in range(i+1, n):
-            soma += matriz_equacao[i][j]* y[j]
-        y[i] = (matriz_equacao[i][n-1] - soma) / matriz_equacao[i][i]   # Fórmula da matriz;
+            soma += A[i][j]* y[j]
+        y[i] = (B[i] - soma) / A[i][i]   # Fórmula da matriz;
 
     return y
 
-matriz = [
-    [-1, 1, 8, 1],
-    [-2, 5, 2, -3],
-    [3, -2, -1, 1],
-    [1, 1, 1, 4]]
-type(matriz)
+
 
 # Resolução do sistema da matriz:
-matriz_solucao = retrosub(matriz)
+matriz_solucao = retrosub()
 
 # Apresentação da solução:
 print("Solução do sistema da matriz:")
