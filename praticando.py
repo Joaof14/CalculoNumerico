61483019ed68398500e844e9c18bae48bd8cba1e
Joaof14 <joao-pedro032011@hotmail.com>
2023-03-30 16:54:50 -0300

    'branch para interpolação'

--- praticando.py
+++ praticando.py
@@ -1,2 +1,14 @@
-#import numpy as np
+import numpy as np
+
+pares= np.array(['1,2','1,2','1,2'])
+x = []
+y = []
+for par in pares:
+    x.append(par.split(',')[0])
+    y.append(par.split(',')[1])
+
+x = np.array(x, dtype=float)
+y = np.array(y, dtype=float)
+print(x)
+print(y)
 
