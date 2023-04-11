b1918172b9ff5daecc5c8bc11c46f2495209abb8
Joaof14 <joao-pedro032011@hotmail.com>
2023-04-11 11:06:01 -0300

    'apresentação amanhã'

--- praticando.py
+++ praticando.py
@@ -54,3 +54,24 @@ def InterpNt(xz,yz):
     print(o)
 #InterpLg(x,y)  
 InterpNt(x,y)
+objtv = int(input("Digite 1 para input de Matriz \nDigite 2 para input de Par ordenado\n"))
+if objtv == 1:
+    outputMatrizesAB(mb = True)
+    metodos = (EliminGauss, FatorLu, Gauss_Jacobi, Gauss_Seidel)
+    func = int(input("método que você quer \n"))
+    output = '\nMétodo de ' + nomes[func] + '\n'
+    outputtxt()
+    metodos[func]()
+else:
+    pares= np.array(['-1,2','0,1','1,2','3,82', '2,17'])
+    xl = []
+    yl = []
+    for par in pares:
+        xl.append(par.split(',')[0])
+        yl.append(par.split(',')[1])
+
+    xl = np.array(xl, dtype=float)
+    yl = np.array(yl, dtype=float)
+    #Interpol(xl,yl)
+    InterpLg(xl,yl,5)
+    InterpNt(xl,yl,5)
